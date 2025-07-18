ec0065c0f96cb022a2c41d33cbe87eaac8399584
Lucas Montecino <lucasmontecino95@gmail.com>
2025-07-18 17:02:37 -0300

    feat: add loading functionality

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,30 @@
-import Landing from "@/components/Landing";
-import Image from "next/image";
+'use client';
+
+import Landing from '@/components/Landing';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export default function LandingPage() {
+  const [isBackendReady, setIsBackendReady] = useState(false);
+
+  useEffect(() => {
+    const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api';
+    // Function to check backend readiness
+    const checkBackend = async () => {
+      try {
+        const res = await fetch(`${API_BASE_URL}/products`);
+        if (res.ok) {
+          setIsBackendReady(true);
+        } else {
+          setTimeout(checkBackend, 1000); // Retry after 1s
+        }
+      } catch {
+        setTimeout(checkBackend, 1000); // Retry after 1s
+      }
+    };
+    checkBackend();
+  }, []);
+
   return (
     <div className="relative min-h-dvh flex flex-col items-center justify-center">
       {/* Background image */}
@@ -11,11 +34,24 @@ export default function LandingPage() {
           alt="Landing background"
           fill
           sizes="100vw"
-          style={{ objectFit: "cover" }}
+          style={{ objectFit: 'cover' }}
           priority
         />
       </div>
-      <Landing />
+      {/* Loading overlay */}
+      {!isBackendReady && (
+        <div className="absolute inset-0 flex flex-col items-center justify-center bg-opacity-40">
+            <Image
+              src="/pointer.gif"
+              alt="Loading..."
+              className="w-fit h-fit object-contain rounded-lg"
+              width={100}
+              height={100}
+            />
+        </div>
+      )}
+      {/* Main content */}
+      {isBackendReady && <Landing />}
     </div>
   );
-} 
+}
